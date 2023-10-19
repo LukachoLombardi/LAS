996894119b901b0e8e26af7497cb8970ab28763b
LukachoLombardi <spamherrre@gmail.com>
2023-10-19 15:11:24 +0200

    Slight refactors and improved Task access

--- LAS.cpp
+++ LAS.cpp
@@ -2,120 +2,130 @@
 #include <Arduino.h>
 
 namespace LAS {
-    Task schedule[SCHEDULE_SIZE] = {Task{
-        false,
-        NULL,
-        0,
-        false,
-        0}
-    };
+Task schedule[SCHEDULE_SIZE] = {Task{
+    false,
+    NULL,
+    0,
+    false,
+    0}
+};
 
-    int determineFirstInactiveIndex(Task array[], int length) {
-        for (int index = 0; index < length; index++) {
-            if (!array[index].isActive) {
-                return index;
-            }
-        }
-        return length;
-    }
+int activeTaskIndex = 0;
 
-    void scheduleFunction(void (*func)(), long triggerTime, bool repeat, int repeatInterval, int remaingRepeats) {
-        schedule[determineFirstInactiveIndex(schedule, SCHEDULE_SIZE)] = Task{
-            true,
-            func,
-            triggerTime,
-            repeat,
-            repeatInterval,
-            remaingRepeats
-        };
-    }
-
-    void scheduleIn(void (*func)(), long triggerDelay) {
-        scheduleFunction(func, millis() + triggerDelay);
-    }
-
-    void scheduleRepeated(void (*func)(), int repeatInterval, int remaingRepeats){
-      scheduleFunction(func, millis() + repeatInterval, true, repeatInterval, remaingRepeats);
-    }
-
-    void printWelcome() {
-        Serial.println("This Unit is running");
-        Serial.println(" __       ______  ____       ");
-        Serial.println("/\\ \\     /\\  _  \\/\\  _`\\     ");
-        Serial.println("\\ \\ \\    \\ \\ \\L\\ \\ \\,\\L\\_\\   ");
-        Serial.println(" \\ \\ \\  __\\ \\  __ \\/_\\__ \\   ");
-        Serial.println("  \\ \\ \\L\\ \\\\ \\ \\/\\ \\/\\ \\L\\ \\ ");
-        Serial.println("   \\ \\____/ \\ \\_\\ \\_\\ `\\____\\\\");
-        Serial.println("    \\/___/   \\/_/\\/_/\\/_____/");
-        Serial.println("");
-        Serial.println("Lukacho's Amazing Scheduler - alpha v0.2.0 - now with repeats!");
-        Serial.println();
-    }
-
-    int activeTaskIndex;
-        void runScheduler() {
-        Serial.begin(FALLBACK_BAUDRATE);
-        Serial.println("starting scheduler...");
-        printWelcome();
-        if(schedule[0].isActive){
-          Serial.println("WARNING: Please consider adding initial Tasks through an ASAP Task for high precision apps");
-        }
-        while (true) {
-            for (int index = 0; index < SCHEDULE_SIZE; index++) {
-                activeTaskIndex = index;
-                Task currentTask = schedule[index];
-                if ((currentTask.isActive) &&
-                  (currentTask.func != NULL) && 
-                (currentTask.triggerTime <= millis())) {
-
-                  if(millis()-currentTask.triggerTime >= CRITICAL_LAG_MS && currentTask.triggerTime != 0){
-                    Serial.println("WARNING: SCHEDULER IS FALLING BEHIND CRITICALLY!");
-                  }
-                  
-                    currentTask.func();
-                    if(currentTask.repeat){
-                      schedule[index].triggerTime = millis() + currentTask.repeatInterval;
-                    } else {
-                      schedule[index].isActive = false;
-                    }
-
-                    if(currentTask.remaingRepeats != ENDLESS_LOOP){
-                      schedule[index].remaingRepeats--;
-                    }
-
-                    if(schedule[index].remaingRepeats == 0) {
-                      schedule[index].isActive = false;
-                    }
-                }
-            }
-        }
-    }
-
-    char* taskToCharStr(Task task) {
-            static char buffer[INTERNAL_CHAR_STR_SIZE_UNIT];
-            snprintf(buffer, sizeof(buffer), "Task:\n  isActive: %d\n  func: %p\n  triggerTime: %d\n  repeat: %d\n  repeatInterval: %d",
-            task.isActive, task.func, task.triggerTime, task.repeat, task.repeatInterval);
-            return buffer;
+int getActiveTaskIndex(){
+  return activeTaskIndex;
 }
-    char* scheduleToCharStr(){
-      static char buffer[INTERNAL_CHAR_STR_SIZE_UNIT*SCHEDULE_SIZE];
-      for(int index=0;index<SCHEDULE_SIZE;index++){
-        static char indexBuffer[3];
-        snprintf(indexBuffer, sizeof(buffer), "\n%d:\n", index);
-        strcat(buffer, indexBuffer);
-        strcat(buffer, taskToCharStr(schedule[index]));
-      }
-      return buffer;
-    }
+Task getActiveTask(){
+  return getTask(activeTaskIndex);
+}
 
-    Task getTask(int index){
-      return schedule[index];
+int determineFirstInactiveIndex(Task array[], int length) {
+  for (int index = 0; index < length; index++) {
+    if (!array[index].isActive) {
+      return index;
     }
+  }
+  return length;
+}
 
-    void printSchedule(){
-      for(int index=0;index<SCHEDULE_SIZE;index++){
-        Serial.print(index); Serial.print(":\n");
-        Serial.println(taskToCharStr(schedule[index]));
+void scheduleFunction(void (*func)(), long triggerTime, bool repeat, int repeatInterval, int remainingRepeats) {
+  schedule[determineFirstInactiveIndex(schedule, SCHEDULE_SIZE)] = Task{
+    true,
+    func,
+    triggerTime,
+    repeat,
+    repeatInterval,
+    remainingRepeats
+  };
+}
+
+void scheduleIn(void (*func)(), long triggerDelay) {
+  scheduleFunction(func, millis() + triggerDelay);
+}
+
+void scheduleRepeated(void (*func)(), int repeatInterval, int repeats) {
+  scheduleFunction(func, millis() + repeatInterval, true, repeatInterval, repeats);
+}
+
+void printWelcome() {
+  Serial.println("This Unit is running");
+  Serial.println(" __       ______  ____       ");
+  Serial.println("/\\ \\     /\\  _  \\/\\  _`\\     ");
+  Serial.println("\\ \\ \\    \\ \\ \\L\\ \\ \\,\\L\\_\\   ");
+  Serial.println(" \\ \\ \\  __\\ \\  __ \\/_\\__ \\   ");
+  Serial.println("  \\ \\ \\L\\ \\\\ \\ \\/\\ \\/\\ \\L\\ \\ ");
+  Serial.println("   \\ \\____/ \\ \\_\\ \\_\\ `\\____\\\\");
+  Serial.println("    \\/___/   \\/_/\\/_/\\/_____/");
+  Serial.println("");
+  Serial.println("Lukacho's Amazing Scheduler - alpha - now with repeats!");
+  Serial.println();
+}
+void runScheduler() {
+  Serial.begin(FALLBACK_BAUDRATE);
+  Serial.println("starting scheduler...");
+  printWelcome();
+  if (schedule[0].isActive) {
+    Serial.println("WARNING: Please consider adding initial Tasks through an ASAP Task for high precision apps");
+  }
+  if (activeTaskIndex != 0) {
+    Serial.println("FATAL: ACTIVETASKINDEX != 0. THERE MIGHT BE ANOTHER SCHEDULER ALREADY RUNNING!");
+  }
+  while (true) {
+    for (int index = 0; index < SCHEDULE_SIZE; index++) {
+      activeTaskIndex = index;
+      Task currentTask = schedule[index];
+      if ((currentTask.isActive) &&
+          (currentTask.func != NULL) &&
+          (currentTask.triggerTime <= millis())) {
+
+        if (millis() - currentTask.triggerTime >= CRITICAL_LAG_MS && currentTask.triggerTime != 0) {
+          Serial.println("WARNING: SCHEDULER IS FALLING BEHIND CRITICALLY!");
+        }
+
+        currentTask.func();
+        if (currentTask.repeat) {
+          schedule[index].triggerTime = millis() + currentTask.repeatInterval;
+        } else {
+          schedule[index].isActive = false;
+        }
+
+        if (currentTask.remainingRepeats != ENDLESS_LOOP) {
+          schedule[index].remainingRepeats--;
+        }
+
+        if (schedule[index].remainingRepeats == 0) {
+          schedule[index].isActive = false;
+        }
       }
     }
+  }
+}
+
+char* taskToCharStr(Task task) {
+  static char buffer[INTERNAL_CHAR_STR_SIZE_UNIT];
+  snprintf(buffer, sizeof(buffer), "Task:\n  isActive: %d\n  func: %p\n  triggerTime: %d\n  repeat: %d\n  repeatInterval: %d",
+           task.isActive, task.func, task.triggerTime, task.repeat, task.repeatInterval);
+  return buffer;
+}
+char* scheduleToCharStr() {
+  static char buffer[INTERNAL_CHAR_STR_SIZE_UNIT * SCHEDULE_SIZE];
+  for (int index = 0; index < SCHEDULE_SIZE; index++) {
+    static char indexBuffer[3];
+    snprintf(indexBuffer, sizeof(buffer), "\n%d:\n", index);
+    strcat(buffer, indexBuffer);
+    strcat(buffer, taskToCharStr(schedule[index]));
+  }
+  return buffer;
+}
+
+Task getTask(int index) {
+  return schedule[index];
+}
+
+void printSchedule() {
+  for (int index = 0; index < SCHEDULE_SIZE; index++) {
+    Serial.print(index); Serial.print(":\n");
+    Serial.println(taskToCharStr(schedule[index]));
+  }
+}
 }
--- LAS.h
+++ LAS.h
@@ -1,28 +1,32 @@
 #pragma once
 
 namespace LAS {
-    #include "LASConfig.h"
+#include "LASConfig.h"
 
-    struct Task {
-        bool isActive; //whether or not a Task is actively maintained. inactive tasks are invalid and can be deleted or overwritten at any time.
-        void (*func)(); //function pointer to the scheduled function
-        long triggerTime; //time in millis to next trigger func
-        //bool triggerASAP; //if true: ignores triggerTime and gets triggered on first discovery in the queue. DISCONTINUED: USE triggerTime = 0
-        bool repeat; //if true: adds repeatIntervall to triggerTime instead of deactivating task right before execution, creating a recurring task.
-        int repeatInterval;
-        int remaingRepeats;
-    };
+struct Task {
+  bool isActive;     //whether or not a Task is actively maintained. inactive tasks are invalid and can be deleted or overwritten at any time.
+  void (*func)();    //function pointer to the scheduled function
+  long triggerTime;  //time in millis to next trigger func
+  //bool triggerASAP; //if true: ignores triggerTime and gets triggered on first discovery in the queue. DISCONTINUED: USE triggerTime = 0
+  bool repeat;  //if true: adds repeatIntervall to triggerTime instead of deactivating task right before execution, creating a recurring task.
+  int repeatInterval;
+  int remainingRepeats;
+};
 
-    extern Task schedule[SCHEDULE_SIZE];
+extern Task schedule[SCHEDULE_SIZE];
+extern int activeTaskIndex;
 
-    int determineFirstFreeIndex(Task array[], int length);
-    void scheduleFunction(void (*func)(), long triggerTime = ASAP, bool repeat = false, int repeatIntervall = 0, int remaingRepeats = -1);
-    void scheduleIn(void (*func)(), long triggerDelay);
-    void scheduleRepeated(void (*func)(), int repeatInterval = ASAP, int remaingRepeats = -1);
-    void printWelcome();
-    Task getTask(int index);
-    char* taskToCharStr(Task task);
-    char* scheduleToCharStr(); //WARNING: VERY MEMORY HUNGRY, WILL PROBABLY CRASH YOUR ARDUINO
-    void printSchedule();
-    void runScheduler();
+int getActiveTaskIndex();
+Task getActiveTask();
+Task getTask(int index);
+
+int determineFirstFreeIndex(Task array[], int length);
+void scheduleFunction(void (*func)(), long triggerTime = ASAP, bool repeat = false, int repeatIntervall = 0, int remainingRepeats = -1);
+void scheduleIn(void (*func)(), long triggerDelay);
+void scheduleRepeated(void (*func)(), int repeatInterval = ASAP, int repeats = -1);
+void printWelcome();
+char* taskToCharStr(Task task);
+char* scheduleToCharStr();  //WARNING: VERY MEMORY HUNGRY, WILL PROBABLY CRASH YOUR ARDUINO
+void printSchedule();
+void runScheduler();
 }
