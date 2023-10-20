07a56f8bc249c5a12a964482536a94d4a9b08332
LukachoLombardi <spamherrre@gmail.com>
2023-10-20 22:45:45 +0200

    quick stash to remote

--- LAS.h
+++ LAS.h
@@ -21,6 +21,7 @@ Task getActiveTask();
 Task getTask(int index);
 
 int determineFirstFreeIndex(Task array[], int length);
+template <typename... Args>
 void scheduleFunction(void (*func)(), long triggerTime = ASAP, bool repeat = false, int repeatIntervall = 0, int remainingRepeats = -1);
 void scheduleIn(void (*func)(), long triggerDelay);
 void scheduleRepeated(void (*func)(), int repeatInterval = ASAP, int repeats = -1);
