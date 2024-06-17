9c5721cf2450bfce8439b36c5f1d360cfd7f51bb
Lukacho <spamherrre@gmail.com>
2024-06-17 20:43:47 +0200

    DEV: Adjust default schedule size

--- LASConfig.h
+++ LASConfig.h
@@ -1,8 +1,11 @@
 #pragma once
 
 struct LASConfig {
-  int scheduleSize = 32;
+  int scheduleSize = 16;
   bool lagWarning = false;
   int maxLagMs = 50;
   int strSize = 128;
+
+  LASConfig(int scheduleSize = 16, bool lagWarning = false, int maxLagMs = 50, int strSize = 128): 
+  scheduleSize(scheduleSize), lagWarning(lagWarning), maxLagMs(maxLagMs), strSize(strSize) {}
 };
